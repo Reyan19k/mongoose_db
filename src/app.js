fc6cb4da84465e02d1020a372286e00da78286d0
Reyan <dinreyanud@gmail.com>
2022-10-13 11:38:13 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+require("./db/connection");
+const mongoose = require('mongoose')
+const yargs = require("yargs")
+const { createMovie, readMovie, updateMovie, deleteMovie } = require("./movie/movieFunctions")
+
+const app = async (yargsObject) => {
+    try {
+        if (yargsObject.create) {
+            await createMovie({ title: yargsObject.title, actor: yargsObject.actor })
+            console.log(await readMovie())
+        } else if (yargsObject.read) {
+            console.log(await readMovie(yargsObject.key, yargsObject.filter));
+        } else if (yargsObject.update) {
+            await updateMovie({ title: yargsObject.title, actor: yargsObject.actor })
+        } else if (yargsObject.delete) {
+            await deleteMovie({ title: yargsObject.title, actor: yargsObject.actor })
+        } else {
+            console.log("Incorrect command")
+        }
+        await mongoose.disconnect()
+    } catch (error) {
+        await mongoose.disconnect()
+        console.log(error)
+    }
+}
+app(yargs.argv)
---
+++ src/movie/movieFunctions.js
@@ -0,0 +1,36 @@
+const Movie = require("./movieModels")
+
+exports.createMovie = async (movieObject) => {
+    try {
+        await Movie.create(movieObject)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.readMovie = async (key, filter) => {
+    try {
+        if (key) {
+            return await Movie.findOne({[key]: filter})
+        } else {
+            return await Movie.find({})
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.updateMovie = async (movieObject) => {
+    try {
+        await Movie.updateOne({title: movieObject.title}, {$set: {actor: movieObject.actor}})
+    } catch (error) {
+        console.log(error)
+    }
+}
+exports.deleteMovie = async (movieObject) => {
+    try {
+        await Movie.deleteOne({title: movieObject.title})
+    } catch (error) {
+        console.log(error)
+    }
+}
